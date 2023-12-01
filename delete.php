cd44167bd37638e3544b51fdea02ec304a04cda5
sahan542 <sahanrashmika542@gmail.com>
2023-12-01 18:01:25 +0530

    crud

---
+++ delete.php
@@ -0,0 +1,27 @@
+<?php
+if ( isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+    $servername = 'localhost';
+    $username = 'root';
+    $password = 'Asdf@1999';
+    $database = 'myshop';
+
+    //create connection
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    $sql = "DELETE FROM clients WHERE id=$id";
+    $result = $connection->query($sql);
+    $connection->query($sql);
+    
+    if ($result) {
+        $successMessage = "Deleted Successfully";
+        header("location: /PHP CRUD/index.php");
+        exit;
+    } else {
+        // Handle the case where the DELETE query fails
+        $errorMessage = "Error deleting record: " . $connection->error;
+    }
+}
+
+?>
